f675bd7136440a5cfce4ef03850285958fe1e2ff
David.Fu <frenchsoftleder@gmail.com>
2023-12-19 11:04:37 +0800

    abt first init commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Deck from './Components/RevealComponents/Deck';
+import Slides from './Slides';
+
+import './index.css';
+import './Themes/override.css';
+import 'reveal.js/dist/theme/moon.css'
+
+
+//const App = () => (
+//  <div className="App">
+//    <Deck>{Slides}</Deck>
+//  </div>
+//);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div  class="reveal">
+      <Deck>{Slides}</Deck>
+    </div>
+  </React.StrictMode>
+);
+
+
+//ReactDOM.render(<App />, document.getElementById('root'));
